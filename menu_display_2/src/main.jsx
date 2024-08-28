b3a0a03e8011cf3e1a8e9dcf9e5d03d4daeba57f
louispaulet <louispaulet13@gmail.com>
2024-08-28 22:39:13 +0200

    add scrollToTop component wrapper to fix the issue with pages starting in the middle

--- menu_display_2/src/main.jsx
+++ menu_display_2/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import ScrollToTop from "./components/scrollToTop";
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </StrictMode>,
