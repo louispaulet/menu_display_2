706c7cf7dcb15a7abe74599e53f9e69e16bc94c7
louispaulet <louispaulet13@gmail.com>
2024-08-29 15:04:03 +0200

    update navbar from single blue to red to orange gradient

--- menu_display_2/src/components/Header.jsx
+++ menu_display_2/src/components/Header.jsx
@@ -2,7 +2,7 @@
 
 function Header() {
   return (
-    <header className="bg-blue-600 text-white p-4">
+    <header className="bg-gradient-to-r from-red-400 via-yellow-500 to-orange-500 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <a href="/" className="text-2xl font-bold">
           <h1>🍽️ Exquisite Menus V2</h1>
